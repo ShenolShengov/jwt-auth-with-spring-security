d24766864c8c93dd7d03d5df596fd7c73a5abb4f
Shenol07 <shenolshengov41@gmail.com>
2024-11-09 10:19:10 +0200

    Add project sturcture

---
+++ src/main/java/bg/about_java/jwt_auth_with_spring_security/JwtAuthWithSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package bg.about_java.jwt_auth_with_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthWithSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthWithSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/about_java/jwt_auth_with_spring_security/JwtAuthWithSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.about_java.jwt_auth_with_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthWithSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
