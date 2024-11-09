9521dfb3d0af8cd50b2dbb948379a27e3aafb503
Shenol07 <shenolshengov41@gmail.com>
2024-11-09 13:12:36 +0200

    Add application.SecurityConfiguration and ApplicationBeanConfiguration

---
+++ src/main/java/bg/about_java/jwt_auth_with_spring_security/config/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package bg.about_java.jwt_auth_with_spring_security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(e -> e
+                        .requestMatchers("/users/register", "/users/login", "/welcome").permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
+    }
+}
